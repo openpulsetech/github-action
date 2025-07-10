ef868c0bc04663afc2a40f2565320f0d0f0e2078
contract-Developer123 <developercontract123@gmail.com>
2025-07-10 22:14:33 +0530

    entry npm

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 echo "👋 Hello $INPUT_WHO_TO_GREET"
 
+npm install
+
 if [ -f /github/workspace/package.json ]; then
   echo "📦 Detected package.json in target repo, installing dependencies..."
   cd /github/workspace
-  npm install
+  # npm install
   yarn install
 else
   echo "📦 No package.json found in target repo, skipping install."
